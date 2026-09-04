9ec3aefb09ea0d7e46778feb26a0f3bd29286a83
Deva Midhun <admin@coder.is-a.dev>
2026-09-04 15:00:33 +0530

    upd

--- src/common/san_pal.c
+++ src/common/san_pal.c
@@ -24,8 +24,44 @@ MISAN_WEAK MISAN_NORETURN void drt_arch_abort(void) {
 }
 
 MISAN_WEAK int drt_arch_map_shadow_memory(uintptr_t addr, size_t size) {
-    (void)addr;
-    (void)size;
+    if (addr == 0) {
+        addr = 0x7fff8000;
+    }
+
+    if (size == 0) {
+        size = 0x100000000000ULL - addr;
+    }
+
+    extern void *mmap(void *addr, size_t length, int prot, int flags, int fd, long offset);
+
+    #define MAP_FAILED ((void *) -1)
+
+    #define MAP_PRIVATE    0x02
+    #define MAP_FIXED      0x10
+    #define MAP_ANON       0x20
+    #define MAP_ANONYMOUS  MAP_ANON
+    #define MAP_NORESERVE  0x4000
+    #define MAP_FIXED_NOREPLACE 0x100000
+
+    #define PROT_READ      1
+    #define PROT_WRITE     2
+
+    void *p = mmap((void *)addr, (size_t)size,
+                   PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE | MAP_FIXED_NOREPLACE,
+                   -1, 0);
+
+    if (p == MAP_FAILED) {
+        p = mmap((void *)addr, (size_t)size,
+                 PROT_READ | PROT_WRITE,
+                 MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE | MAP_FIXED,
+                 -1, 0);
+    }
+
+    if (p == MAP_FAILED) {
+        return -1;
+    }
+
     return 0;
 }
 
